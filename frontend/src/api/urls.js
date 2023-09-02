ecd34b05a090d798c2bd0b17a247be5a50fc2bb5
Brayden <brayden.tam@gmail.com>
2023-09-02 00:25:59 -0700

    added env for domain

---
+++ frontend/src/api/urls.js
@@ -0,0 +1,27 @@
+const DOMAIN = process.env.REACT_APP_DOMAIN;
+//const DOMAIN = "http://localhost:4000";
+
+//AUTHENTICATION
+export const LOGIN = DOMAIN + "/user/login";
+export const SIGNUP = DOMAIN + "/user/signup";
+
+//BUZZES
+export const GETEXPLORE = DOMAIN + "/buzz/getAll";
+export const CREATEBUZZ = DOMAIN + "/buzz/create";
+export const GETBYID = DOMAIN + "/buzz/getById";
+export const GETBYUSERNAME = DOMAIN + "/buzz/getByUsername";
+export const LIKE = DOMAIN + "/buzz/like";
+export const GETFOLLOWINGBUZZES = DOMAIN + "/buzz/getFollowing";
+export const GETCOMMENTS = DOMAIN + "/buzz/getComments";
+export const DELETE = DOMAIN + "/buzz/delete";
+export const HASPOSTED = DOMAIN + "/buzz/hasPosted";
+
+//PROFILES
+export const GETPROFILE = DOMAIN + "/profile/getProfile";
+export const FOLLOW = DOMAIN + "/profile/follow";
+export const GETFOLLOWERS = DOMAIN + "/profile/getFollowers";
+export const GETFOLLOWING = DOMAIN + "/profile/getFollowing";
+export const SEARCH = DOMAIN + "/profile/search";
+
+//TODO: SORT THESE (and properly space), AND SORT REQUESTS.JS TO FOLLOW
+//TODO: https://blog.dreamfactory.com/best-practices-for-naming-rest-api-endpoints/
